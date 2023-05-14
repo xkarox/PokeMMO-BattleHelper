b0a70655920c10f934b53fe8f221e27016cfc36f
Adrian Grzybek <92431880+xkarox@users.noreply.github.com>
2023-05-14 22:04:17 +0200

    clean up and api wrapper change

--- main.py
+++ main.py
@@ -5,33 +5,27 @@ import keyboard
 import cv2
 
 def main():
-    # list = db.get_pokemon_types("oddish")
-    
-    # print(list)
     gds = game_data_service()
     db = Database_service()
-    # gastly = db.get_pokemon("gastly")
-    
-    # print(gastly)
-    # print(type(gastly))
-    
-    # print(db.get_pokemon_types(gastly))
-    
+
     window_handle = gds.find_window()
     while True:
         if (keyboard.is_pressed("9")):
-            print("pressed")
+            print("Key pressed")
             img = gds.capture_screen( window_handle, showWindow=False)
             data = gds.process_image(img)
             found_pokemon = gds.find_pokemon_names(data)
-            # found_pokemon = ['oddish', 'gastly', 'starmie']
-            # if len(found_pokemon) != 0:
-            print(len(found_pokemon))
-            for pokemon in found_pokemon:
-                # gastly = db.get_pokemon("oddish")
-                db_pokemon = db.get_pokemon(pokemon)
-                types = db.get_pokemon_types(db_pokemon)
-                print(f'Pokemon: {db_pokemon}\nTypes: {types}')
+
+            print(f'Found pokemon: {len(found_pokemon)}')
+            
+            if len(found_pokemon) != 0:
+            
+                for pokemon in found_pokemon:
+
+                    db_pokemon = db.get_pokemon(pokemon)
+                    types = db.get_pokemon_types(db_pokemon)
+                    
+                    print(f'Pokemon: {db_pokemon}\nTypes: {types}')
             
             #close the window when pressing 'q'      
             if (cv2.waitKey(1) & 0xFF) == ord('q'):
